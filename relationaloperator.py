a67bb42c11f2ad3897f14132761fe66089de932b
noorulhuda0604-hue <noorulhuda0604@gmail.com>
2026-05-17 16:29:49 +0500

    Add files via upload

---
+++ relationaloperator.py
@@ -0,0 +1,16 @@
+# Relational operator 
+a = 10
+b = 5
+print(a > b)    # True
+print(a < b)    # False
+print(a == b)   # False
+print(a != b)   # True
+print(a >= b)   # True
+print(a <= b)   # False
+# user input 
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+print("Greater:", num1 > num2)
+print("Equal:", num1 == num2)
+print("Not Equal:", num1 != num2)
